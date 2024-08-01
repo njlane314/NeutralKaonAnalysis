cae662afbda88ba06c59d12c162d191745ed38e8
Nick Lane <njlane314@gmail.com>
2024-08-01 08:56:28 +0100

    Initial commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
-setup uboonecode v08_00_00_69 -q e17:prof 
+setup uboonecode v08_00_00_81 -q e17:prof 
 
 export HYP_TOP=$PWD/
 
 export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:$PWD/Core:$PWD/Alg:$PWD/ParameterSets:$PWD/SampleSets
-export DATA_DIR=/uboone/data/users/cthorpe/K0SAnalysis/
+export DATA_DIR=/exp/uboone/data/users/cthorpe/K0SAnalysis/
